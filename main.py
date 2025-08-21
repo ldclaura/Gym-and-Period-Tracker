936386b8a4446dff8e60aafb5b17a30023bf403b
laura <lauracain12345@gmail.com>
2025-08-21 15:28:37 +1000

    mess with sqlite, db files, created db, etc

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import requests
 import os
 import csv
+import sqlite3
+from sqlite3 import OperationalError
 import  pandas as pd
 #need dotennv and load_dotenv for environment variable
 #pip install python-dotenv
@@ -10,15 +12,24 @@ load_dotenv()
 #NOTE use sqlite sqlalchemy, sort csv as db instead. look at day 63 for how to do it
 
 file = 'megaGymDataset.csv'
+sqlfile = 'megaGymDataset.sql'
+dbfile = 'gymdataset.db'
 
-with open(file) as data_file:
-        data = csv.reader(data_file)
-        temperatures = [] #integers not string
-        for row in data:
-            print(row)
 
-panda_data = pd.read_csv(file)
-print(panda_data["Title"]) #prints temp column
+
+
+
+def pandy():
+    # ,Title,Desc,Type,BodyPart,Equipment,Level,Rating,RatingDesc
+    panda_data = pd.read_csv(file)
+    print(panda_data["Title"]) #prints exercise column
+    print(panda_data["Desc"]) #prints temp column
+    print(panda_data["BodyPart"]) #prints temp column
+    print(panda_data["Equipment"]) #prints temp column
+    print(panda_data["Level"]) #prints temp column
+    print(panda_data["Rating"]) #prints temp column
+    print(panda_data["RatingDesc"]) #prints temp column
+
 
 #APISs
 #FoodData central, national nutrient database for standard reference https://fdc.nal.usda.gov/
